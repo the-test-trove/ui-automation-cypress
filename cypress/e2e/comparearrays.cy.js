3a2667cabf15df9d72e72cb8120f469289dfcdfe
mudit94 <mudit94@gmail.com>
2022-07-28 19:35:24 +0530

    test(e2e): fixed with same array contents

--- cypress/e2e/comparearrays.cy.js
+++ cypress/e2e/comparearrays.cy.js
@@ -1,6 +1,6 @@
 describe('Compare Arrays', () => {
     var a = [1, 2, 3, 9];
-    var b = [1, 3, 4, 9];
+    var b = [1, 2, 3, 9];
     it('Test to show difference in arrays', () => {
         expect(a).to.eql(b);
     })
